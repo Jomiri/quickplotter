09a4336306a01674f980a4ff77372b9248849a86
Joona Rissanen <joona.m.rissanen@gmail.com>
2018-07-08 18:46:59 +0300

    Added sidebar and customization controls

--- plotter.js
+++ plotter.js
@@ -1,18 +1,44 @@
-var margin = {top: 20, right: 40, bottom: 50, left: 100};
+var margin = {top: 50, right: 40, bottom: 100, left: 100};
 var axMargin = {x: 0.0, y: 0.05};
 var selector = '#figure_area';
+
+const defaultPlotStyle = {
+    'plotType': 'line',
+    'majorTickSize': 7,
+    'minorTickSize': 4,
+    'axisStrokeWidth': '3px',
+    'axisFontSize': '16px',
+    'xLabelFontSize': '20px',
+    'yLabelFontSize': '20px',
+    'titleFontSize': '24px',
+    'xScaling': 1.0,
+    'yScaling': 1.0,
+    'xStart': 'auto',
+    'xEnd': 'auto',
+    'yStart': 'auto',
+    'yEnd': 'auto',
+    'scatterDotRadius': 5,
+    'lineStrokeWidth': '2px', 
+    'dataColor': 'black'
+}
+
+var currentPlotStyle = Object.assign({}, defaultPlotStyle);
+
+/*
 var majorTickSize = 7;
 var minorTickSize = 4;
 var axisStrokeWidth = 3;
 var axisFontSize = '1em';
-var axisFont = 'Sans-Serif';
-var canvasResFactor = 2;
-var imageSizeFraction = 0.8;
 var lineStrokeWidth = '2px';
-var nTicks = 5;
 var labelFontSize = '1.5em';
+*/
 var xyG = [];
 var fileNameG = '';
+var canvasResFactor = 4;
+var imageWidthFraction = 0.7;
+var imageHeightFraction = 1;
+var axisFont = 'Sans-Serif';
+var nTicks = 5;
 
 // https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
 function dropHandler(ev) {
@@ -53,6 +79,7 @@ function fileHandler(file) {
 function parseAndPlot(str, fileName) {
     var xy = regexParse(str);
     saveTempData(xy, fileName);
+    resetLimits();
     plotXY(xy, margin, axMargin, selector, fileName);
     hideInstruction(selector);
 }
@@ -80,26 +107,45 @@ function redraw(selector) {
     plotXY(xyG, margin, axMargin, selector, fileNameG);
 }
 
+function scaleArray(arr, multiplier) {
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] *= multiplier;
+    } 
+}
+
+function getCoordinateLimits(dataArr, margin, userLimits) {
+    var span = getAxSpan(dataArr);
+    var lim = [d3.min(dataArr) - span * margin, d3.max(dataArr) + span * margin];
+    if (userLimits[0] !== 'auto') {
+        lim[0] = toFloat(userLimits[0]);
+    }
+    if (userLimits[1] !== 'auto') {
+        lim[1] = toFloat(userLimits[1]);
+    }
+    return lim;
+}
+
 function plotXY(xy, margin, axMargin, selector, fileName) {
 
     if (xy.length === 0) { return; }
 
     resetFigure();
+    updatePlotStyle();
 
     var size = getAxisSize(margin);
     var width = size.width;
     var height = size.height;
 
-    var x = xy[0];
-    var y = xy[1];
+    var x = xy[0].slice();
+    var y = xy[1].slice();
+
+    scaleArray(x, currentPlotStyle['xScaling']);
+    scaleArray(y, currentPlotStyle['yScaling']);
+    
     var lineData = makeLineData(x, y);
 
-    var xSpan = getAxSpan(x);
-    var ySpan = getAxSpan(y);
-
-    var xLim = [d3.min(x) - xSpan * axMargin.x, d3.max(x) + xSpan * axMargin.x];
-    var yLim = [d3.min(y) - ySpan * axMargin.y, d3.max(y) + ySpan * axMargin.y];
-
+    var xLim = getCoordinateLimits(x, axMargin.x, [currentPlotStyle['xStart'], currentPlotStyle['xEnd']]);
+    var yLim = getCoordinateLimits(y, axMargin.y, [currentPlotStyle['yStart'], currentPlotStyle['yEnd']]);
 
     var xScale = d3.scaleLinear()
                     .domain(xLim)
@@ -163,47 +209,46 @@ function plotXY(xy, margin, axMargin, selector, fileName) {
         .tickSize(0, 0)
         .tickValues(yTickValues);
 
+
+
+    ax.append('clipPath')
+    .attr('id', 'clip_path')
+    .append('rect')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', width)
+        .attr('height', height);
+    
+    drawData(lineData, ax, xScale, yScale);
+
     // Add axes to the ax group
     ax.append('g')
     .attr('transform', 'translate(0,' + height + ')')
     .attr('class', 'x_axis')
-    .attr('stroke-width', axisStrokeWidth)
+    .attr('stroke-width', currentPlotStyle['axisStrokeWidth'])
     .style('font-family', axisFont)
-    .style('font-size', axisFontSize)
+    .style('font-size', currentPlotStyle['axisFontSize'])
     .call(xAxis);
 
     ax.append('g')
     .attr('transform', 'translate(0,0)')
     .attr('class', 'y_axis')
-    .attr('stroke-width', axisStrokeWidth)
+    .attr('stroke-width', currentPlotStyle['axisStrokeWidth'])
     .style('font-family', axisFont)
-    .style('font-size', axisFontSize)
+    .style('font-size', currentPlotStyle['axisFontSize'])
     .call(yAxis);
 
     ax.append('g')
     .attr('transform', 'translate(0,0)')
     .attr('class', 'x_axis_top')
-    .attr('stroke-width', axisStrokeWidth)
+    .attr('stroke-width', currentPlotStyle['axisStrokeWidth'])
     .call(xAxisTop);
 
     ax.append('g')
     .attr('transform', 'translate(' + width + ',0)')
     .attr('class', 'y_axis_right')
-    .attr('stroke-width', axisStrokeWidth)
+    .attr('stroke-width', currentPlotStyle['axisStrokeWidth'])
     .call(yAxisRight);
-
-    // Define the plot type - here line
-    var drawLine = d3.line()
-        .x(function(d) { return xScale(d.x_coord); })
-        .y(function(d) { return yScale(d.y_coord); });
-
-    // Add the plotted curve as path
-    ax.append('path')
-    .attr('d', drawLine(lineData))
-    .attr('class', 'curve')
-    .attr('fill', 'none')
-    .attr('stroke', 'black')
-    .attr('stroke-width', lineStrokeWidth);
 
     // Major and minor tick line formatting
     var xTickLines = d3.selectAll('.x_axis .tick line');
@@ -211,6 +256,8 @@ function plotXY(xy, margin, axMargin, selector, fileName) {
     var yTickLines = d3.selectAll('.y_axis .tick line');
     var yTickLinesRight = d3.selectAll('.y_axis_right .tick line');
 
+    var majorTickSize = currentPlotStyle['majorTickSize'];
+    var minorTickSize = currentPlotStyle['minorTickSize'];
     xTickLines.attr('y2', function (d, i) { return (i % 2 === 0) ? -majorTickSize : -minorTickSize; });
     xTickLinesTop.attr('y2', function (d, i) { return (i % 2 === 0) ? majorTickSize : minorTickSize; });
     yTickLines.attr('x2', function (d, i) { return (i % 2 === 0) ? majorTickSize : minorTickSize; });
@@ -236,6 +283,55 @@ function plotXY(xy, margin, axMargin, selector, fileName) {
     yTickLabels.attr('transform', 'translate(-5,0)');
     addXLabel(document.getElementById('xLabel').value);
     addYLabel(document.getElementById('yLabel').value);
+    addTitle(document.getElementById('title').value);
+}
+
+function drawData(dataPoints, ax, xScale, yScale) {
+
+    var plotType = currentPlotStyle['plotType'];
+    if (plotType === 'line') {
+        var drawFunc = d3.line()
+            .x(function(d) { return xScale(d.x_coord); })
+            .y(function(d) { return yScale(d.y_coord); });
+        // Add the plotted curve as path
+        ax.append('path')
+        .attr('d', drawFunc(dataPoints))
+        .attr('class', 'curve')
+        .attr('fill', 'none')
+        .attr('stroke', currentPlotStyle['dataColor'])
+        .attr('clip-path', 'url(#clip_path)')
+        .attr('stroke-width', currentPlotStyle['lineStrokeWidth']);
+
+    } else if (plotType === 'scatter') {
+        var scatterPlot = ax.append('g')
+        .attr('class', 'curve')
+        .attr('id', 'scatterPlot')
+        .attr('clip-path', 'url(#clip_path)');
+
+        scatterPlot.selectAll('scatter-dots')
+        .data(dataPoints)
+        .enter().append('circle')
+            .attr('class', 'scatter_dot')
+            .attr('cx', function (d) { return xScale(d.x_coord); })
+            .attr('cy', function (d) { return yScale(d.y_coord); })
+            .attr('r', currentPlotStyle['scatterDotRadius'])
+            .attr('fill', currentPlotStyle['dataColor']);
+    }
+}
+function addTitle(titleText) {
+    var size = getAxisSize(margin);
+    var width = size.width;
+    var center = Math.floor(width / 2);
+    var axis = d3.select('#ax');
+    axis.selectAll('.title').remove();
+    axis.append('text')
+    .attr('class', 'title')
+    .attr('text-anchor', 'middle')
+    .attr('x', center)
+    .attr('y', -20)
+    .attr('font-family', axisFont)
+    .attr('font-size', currentPlotStyle['titleFontSize'])
+    .text(titleText);
 }
 
 function addXLabel(labelText) {
@@ -249,9 +345,9 @@ function addXLabel(labelText) {
     .attr('class', 'x_label')
     .attr('text-anchor', 'middle')
     .attr('x', center)
-    .attr('y', height + 45)
+    .attr('y', height + 50)
     .attr('font-family', axisFont)
-    .attr('font-size', labelFontSize)
+    .attr('font-size', currentPlotStyle['xLabelFontSize'])
     .text(labelText);
 }
 
@@ -267,11 +363,11 @@ function addYLabel(labelText) {
     .attr('class', 'y_label')
     .attr('text-anchor', 'middle')
     .attr('dominant-baseline', 'central')
-    .attr('y', -90)
+    .attr('y', -75)
     .attr('x', -center)
     .attr('dy', '0.75em')
     .attr('font-family', axisFont)
-    .attr('font-size', labelFontSize)
+    .attr('font-size', currentPlotStyle['yLabelFontSize'])
     .text(labelText);
 }
 
@@ -355,8 +451,8 @@ function getSvgFigure() {
 // Helper funcs
 
 function getAxisSize(margin) {
-    var figHeight = Math.floor(window.innerHeight * imageSizeFraction);
-    var figWidth = Math.floor(window.innerWidth * imageSizeFraction);
+    var figHeight = document.getElementById('figure_area').offsetHeight;//Math.floor(window.innerHeight * imageHeightFraction);
+    var figWidth = document.getElementById('figure_area').offsetWidth;//Math.floor(window.innerWidth * imageWidthFraction);
     var width = figWidth - margin.left - margin.right
     var height = figHeight - margin.top - margin.bottom;
     return {width: width, height: height};
@@ -394,16 +490,23 @@ function addMinorTicks(majorTicks, limits) {
 
 function getTickFormat(ticks) {
     var range = ticks[ticks.length - 1] - ticks[0];
-    var orderOfMagn = Math.floor(Math.log10(Math.abs(range)));
+    var orderOfMagn = Math.floor(Math.log10(Math.max(Math.abs(ticks[0]), Math.abs(ticks[ticks.length-1]))));
     var formatString = '';
+    console.log(orderOfMagn);
     if (ticks.every(numIsInteger)) {
         formatString = 'd';
     }
-    else if (Math.abs(orderOfMagn) < 3) {
+    else if (orderOfMagn < -2) {
+        formatString = '.1e';
+    }
+    else if (orderOfMagn <= 0) {
         formatString = '.' + (Math.abs(orderOfMagn)+1) + 'f';
     }
+    else if (orderOfMagn < 4) {
+        formatString = '.1f';
+    }
     else {
-        formatString = '.2e';
+        formatString = '.1e';
     }
     return d3.format(formatString);
 }
@@ -431,11 +534,111 @@ function numIsInteger(num) {
     return num % 1 === 0;
 }
 
+function appendStrtoArr(arr, str) {
+    var output = [];
+    for (var i = 0; i < arr.length; i++) {
+        output.push(arr[i] + str);
+    }
+    return output;
+}
+
+function initSideBar() {
+    var fontSizesInt = d3.range(10, 52 ,2);
+    var fontSizesStr = appendStrtoArr(fontSizesInt, 'px');
+
+    var strokeWidths = appendStrtoArr(d3.range(0.5, 5.5, 0.5), 'px');
+    var dotRadii = d3.range(1, 10, 0.5);
+    
+
+
+    populateSelectBox('xFontSize', fontSizesStr);
+    populateSelectBox('yFontSize', fontSizesStr);
+    populateSelectBox('titleFontSize', fontSizesStr);
+    populateSelectBox('lineStrokeWidth', strokeWidths);
+    populateSelectBox('scatterDotRadius', dotRadii);
+
+    document.getElementById('xFontSize').value = defaultPlotStyle['xLabelFontSize'];
+    document.getElementById('yFontSize').value = defaultPlotStyle['yLabelFontSize'];
+    document.getElementById('titleFontSize').value = defaultPlotStyle['titleFontSize'];
+    document.getElementById('xScaling').value = defaultPlotStyle['xScaling'];
+    document.getElementById('yScaling').value = defaultPlotStyle['yScaling'];
+    document.getElementById('lineStrokeWidth').value = defaultPlotStyle['lineStrokeWidth']
+    document.getElementById('scatterDotRadius').value = defaultPlotStyle['scatterDotRadius']
+    
+    resetLimits();
+
+    document.getElementById('plotType').value = defaultPlotStyle['plotType'];
+
+
+    document.getElementById('svgButton').addEventListener('click', downloadSVG);
+    document.getElementById('pngButton').addEventListener('click', downloadPNG);
+
+    document.getElementById('xFontSize').addEventListener('change', function(event) {redraw(selector);});
+    document.getElementById('yFontSize').addEventListener('change', function(event) {redraw(selector);});
+    document.getElementById('titleFontSize').addEventListener('change', function(event) {redraw(selector);});
+
+    document.getElementById('xScaling').addEventListener('change', function(event) {redraw(selector);});
+    document.getElementById('yScaling').addEventListener('change', function(event) {redraw(selector);});
+
+    document.getElementById('xStart').addEventListener('change', function(event) {redraw(selector);});
+    document.getElementById('xEnd').addEventListener('change', function(event) {redraw(selector);});
+
+    document.getElementById('yStart').addEventListener('change', function(event) {redraw(selector);});
+    document.getElementById('yEnd').addEventListener('change', function(event) {redraw(selector);});
+
+    document.getElementById('plotType').addEventListener('change', function (event) { redraw(selector); });
+    document.getElementById('dataColor').addEventListener('change', function (event) { redraw(selector); });
+
+    document.getElementById('lineStrokeWidth').addEventListener('change', function (event) { redraw(selector); });
+    document.getElementById('scatterDotRadius').addEventListener('change', function (event) { redraw(selector); });
+
+    document.getElementById('xLabel').addEventListener('input', function (event) {
+        addXLabel(document.getElementById('xLabel').value); })
+    document.getElementById('yLabel').addEventListener('input', function (event) {
+        addYLabel(document.getElementById('yLabel').value); })
+    document.getElementById('title').addEventListener('input', function (event) {
+        addTitle(document.getElementById('title').value); })
+}
+
+function resetLimits() {
+    document.getElementById('xStart').value = defaultPlotStyle['xStart'];
+    document.getElementById('xEnd').value = defaultPlotStyle['xEnd'];
+    document.getElementById('yStart').value = defaultPlotStyle['yStart'];
+    document.getElementById('yEnd').value = defaultPlotStyle['yEnd'];
+}
+
+//https://stackoverflow.com/questions/9895082/javascript-populate-drop-down-list-with-array
+function populateSelectBox(idSelector, optionArray) {
+    var box = document.getElementById(idSelector);
+    for(var i = 0; i < optionArray.length; i++) {
+        var option = optionArray[i];
+        var el = document.createElement('option');
+        el.textContent = option;
+        el.value = option;
+        box.appendChild(el);
+    }
+}
+
+function updatePlotStyle() {
+    currentPlotStyle['xLabelFontSize'] = document.getElementById('xFontSize').value; 
+    currentPlotStyle['yLabelFontSize'] = document.getElementById('yFontSize').value; 
+    currentPlotStyle['titleFontSize'] = document.getElementById('titleFontSize').value; 
+
+    currentPlotStyle['xScaling'] = toFloat(document.getElementById('xScaling').value); 
+    currentPlotStyle['yScaling'] = toFloat(document.getElementById('yScaling').value); 
+
+    currentPlotStyle['xStart'] = document.getElementById('xStart').value; 
+    currentPlotStyle['xEnd'] = document.getElementById('xEnd').value; 
+
+    currentPlotStyle['yStart'] = document.getElementById('yStart').value; 
+    currentPlotStyle['yEnd'] = document.getElementById('yEnd').value; 
+
+    currentPlotStyle['plotType'] = document.getElementById('plotType').value;
+    currentPlotStyle['dataColor'] = document.getElementById('dataColor').value;
+    currentPlotStyle['lineStrokeWidth'] = document.getElementById('lineStrokeWidth').value;
+    currentPlotStyle['scatterDotRadius'] = document.getElementById('scatterDotRadius').value;
+}
+
+initSideBar();
 document.getElementById('figure_area').addEventListener('paste', readPasteAndPlot);
-document.getElementById('xLabel').addEventListener('input', function (event) {
-    addXLabel(document.getElementById('xLabel').value); })
-document.getElementById('yLabel').addEventListener('input', function (event) {
-    addYLabel(document.getElementById('yLabel').value); })
-document.getElementById('svgButton').addEventListener('click', downloadSVG);
-document.getElementById('pngButton').addEventListener('click', downloadPNG);
 window.addEventListener('resize', function(event) {redraw(selector);});
